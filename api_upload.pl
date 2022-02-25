0701ea0aee201be3733b83448b2625245b94a8fd
Toshiyuki SHIMONO <bin4tsv@gmail.com>
2022-02-25 21:20:43 +0900

    api_upload.pl 設定ファイル方式に対応。そして、複数ファイルに対応。ただし、このプログラムはテストしていない。

--- api_upload.pl
+++ api_upload.pl
@@ -1,42 +1,36 @@
 #!/usr/bin/perl
 use 5.026 ; use strict; use warnings;
-binmode STDOUT, ":utf8";
-
 use HTTP::Request::Common;
 use JSON qw/encode_json/;
 use LWP::UserAgent;
 use Getopt::Std ; getopts 'f:m:' , \my%o ;
-
+binmode STDOUT, ":utf8";
 $o{f} //= '' ; # フォルダ名
-$o{m} //= 'plain/text' ; # 
-chomp (my $ACCESS_TOKEN = $ARGV[0] ) ; 
+$o{m} //= 'plain/text' ; # MIMEタイプ
+my $gfile = '~/.gcpsetup2202/1' ; # GCPで使う合言葉を収めたファイルの名前
+my $ACCESS_TOKEN = qx [ sed -ne's/^ACCESS_TOKEN[ =:\t]*//p' $gfile ] =~ s/\n$//r ;
+chomp ( $ACCESS_TOKEN = <> ) if $o{'/'} ; #
 my $GOOGLE_DRIVE_UPLOAD_API = "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart" ;
+& f_each ( $_ ) for @ARGV ; 
+exit ;
 
-my $mimeType = $o{m} ; 
-my $ej = { name => $ARGV[1] , mimeType => $mimeType , $o{f} ne q[] ? (parents  => [ $o{f} ] ): () } ;
-my $EJ = encode_json $ej  ; 
-
-my $ua = LWP::UserAgent->new;
-my $res = $ua->request(
-  POST $GOOGLE_DRIVE_UPLOAD_API,
-  'Content-Type' => 'multipart/form-data',
-  Authorization =>  "Bearer $ACCESS_TOKEN",
-  Content => [
-    metadata => [
-      undef,
-      undef,
-      'Content-Type' => 'application/json;charset=UTF-8',
-      'Content' => $EJ  #encode_json( %EJ ),
-    ],
-    file => [ $ARGV[1] ],
-  ],
-);
-
-print  "\$res->code = ", $res->code . "\n";
-print  $res->content . "\n";
-
-
-
+sub f_each ( $ ) { 
+  my $mimeType = $o{m} ; 
+  my $ej0 = { name => $ARGV[1] , mimeType => $mimeType , $o{f} ne q[] ? ( parents  => [ $o{f} ] ): () } ;
+  my $ej1 = encode_json $ej0 ; 
+  my $ua = LWP::UserAgent->new ;
+  my $res = $ua -> request (
+    POST $GOOGLE_DRIVE_UPLOAD_API ,
+    'Content-Type' => 'multipart/form-data' ,
+    Authorization =>  "Bearer $ACCESS_TOKEN" ,
+    Content => [
+      metadata => [ undef, undef , 'Content-Type' => 'application/json;charset=UTF-8' , 'Content' => $ej1 ] ,
+      file => [ $ARGV[1] ] ,
+    ] ,
+  ) ;
+  print  "\$res->code = ", $res->code . "\n";
+  print  $res->content . "\n";
+}
 
 ## ヘルプ (オプション --help が与えられた時に、動作する)
 sub VERSION_MESSAGE {}
@@ -56,17 +50,18 @@ sub HELP_MESSAGE {
 
 =head1
 
- $0  -f 目的のフォルダID アクセストークン ファイル名 ..
+ $0  -f 目的のフォルダID ファイル名 [ファイル名] [ファイル名] ..
 
-  2番目はローカルのファイル名
-  指定したファイルを指定したGoogleドライブのフォルダーにアップロードする。
-  同じ名前のファイルも複数回、このプログラムを実行すると、新規に次々とGoogleドライブにアップロードされる。少し要注意。
+  指定したファイルを指定したGoogleドライブのフォルダにアップロードする。
+  (同じ名前のファイルも複数回、このプログラムを実行すると、新規に次々とGoogleドライブにアップロードされる。少し要注意。)
 
- 前提的なこと: HTTP::Request::Common を用いる。Net::Google::OAuthを使わない。
+  このプログラムは HTTP::Request::Common を用いていて、Net::Google::OAuthを使わない。
 
   オプション: 
+    -/     : アクセストークンを標準入力から読み取る。
     -f STR : 指定しないか、空文字だと、グーグル直下のディレクトリになる。
     -m TYPE : text/csv　などを指定。 未指定なら plain/text ;
+
 
 出力例: 
 
