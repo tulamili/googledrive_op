fc1aa41474013af4604623b5074bff8b38b1ad5e
Toshiyuki SHIMONO <bin4tsv@gmail.com>
2022-02-25 21:41:52 +0900

    api_dirs.pl -D を実装。JSON形式で取ってきたデータを解読して、Dumperで中を見るようにする。

--- api_dirs.pl
+++ api_dirs.pl
@@ -1,8 +1,9 @@
 #!/usr/bin/env perl
 use 5.030 ; use strict; use warnings;
+use Data::Dumper ; 
+use Getopt::Std ; getopts 'g:/D' , \my%o ;
 use HTTP::Tiny ;
 use JSON ; use URI::Escape ; use URI ;
-use Getopt::Std ; getopts 'g:/' , \my%o ;
 binmode STDOUT, ":utf8";
 
 my $GOOGLE_DRIVE_API = "https://www.googleapis.com/drive/v3/files" ;
@@ -19,25 +20,27 @@ $uri -> query_form ( access_token => $ACCESS_TOKEN ) ;
 & files ( $uri ) ;
 
 sub files {
-    my $uri   = shift;
-    my $count = 0 ; # URIの中身から取り出した nextPageToken を引っ張り出した回数
-    my $fnum = 0 ; # ファイルの個数
-    my $ht    = HTTP::Tiny->new();
-    while ( $count < $count_limit ) {
-        my $contents = decode_json( $ht->get($uri)->{content} );
-        $uri->query_form( access_token => $ACCESS_TOKEN, pageToken => $contents->{nextPageToken} ) ;
-        for my $content ( @{ $contents->{files} } ) {
-            print  sprintf ("%05d ", ++ $fnum ) . "=" x 20 . "\n" ;
-            printf( "%-8s: %s\n", "id",       $content->{id} );
-            printf( "%-8s: %s\n", "name",     $content->{name} );
-            printf( "%-8s: %s\n", "mimeType", $content->{mimeType} );
-            printf( "%-8s: %s\n", "kind",     $content->{kind} );
-            print "=" x 20 . "\n";
-        }
-        $count ++ ;
-        last if ! $contents->{nextPageToken};
-        # 最終ページには nextPageToken キーが無い
+  my $uri = shift;
+  my $count = 0 ; # URIの中身から取り出した nextPageToken を引っ張り出した回数
+  my $fnum = 0 ; # ファイルの個数
+  my $ht = HTTP::Tiny->new();
+  while ( $count < $count_limit ) {
+    my $contents = decode_json( $ht->get($uri)->{content} );
+    do { print Dumper $contents ; $contents->{error} ? last : next } if $o{D} ;
+    $uri->query_form( access_token => $ACCESS_TOKEN, pageToken => $contents->{nextPageToken} ) ;
+    for my $content ( @{ $contents->{files} } ) {
+      print  sprintf ("%05d ", ++ $fnum ) . "=" x 20 . "\n" ;
+      printf( "%-8s: %s\n", "id",     $content->{id} );
+      printf( "%-8s: %s\n", "name",   $content->{name} );
+      printf( "%-8s: %s\n", "mimeType", $content->{mimeType} );
+      printf( "%-8s: %s\n", "kind",   $content->{kind} );
+      print "=" x 20 . "\n";
     }
+    last if ! $contents->{nextPageToken}; # 最終ページには nextPageToken キーが無い
+  }
+  continue { 
+    $count ++ ;
+  }
 }
 
 ## ヘルプ (オプション --help が与えられた時に、動作する)
@@ -47,8 +50,8 @@ sub HELP_MESSAGE {
   $ARGV[1] //= '' ;
   open my $FH , '<' , $0 ;
   while(<$FH>){
-    s/\$0/$Script/g ;
-    print $_ if s/^=head1// .. s/^=cut// and $ARGV[1] =~ /^o(p(t(i(o(ns?)?)?)?)?)?$/i ? m/^\s+\-/ : 1;
+  s/\$0/$Script/g ;
+  print $_ if s/^=head1// .. s/^=cut// and $ARGV[1] =~ /^o(p(t(i(o(ns?)?)?)?)?)?$/i ? m/^\s+\-/ : 1;
   }
   close $FH ;
   exit 0 ;
@@ -60,13 +63,13 @@ sub HELP_MESSAGE {
 
  $0 
 
- 前提的なこと: HTTP::Tinyを用いる。Net::Google::OAuthを使わない。
-
- アクセストークンを標準入力に与えたら、ドライブのファイルの一覧を出力。
+ ドライブのファイルの一覧を出力。何らかの手段でこのプログラムにアクセストークンを与えることが必要。
 
 　オプション : 
-    -g N : 何回ページをたぐるか? 未指定なら2。
-    -/   : アクセストークンは、標準入力から読み取る。設定ファイルからではなく。
+  -/   : アクセストークンは、標準入力から読み取る。設定ファイルからではなく。
+  -g N : 何回ページをたぐるか? 未指定なら2。
+  -D   : 取ってきたデータを Dumper で出力する。エラーが起きたときの様子を調べるのに便利。
 
 その他 : 
-  1万個ファイルがあると、全部見せるのに、1分間の時間がかかるであろう。
+  - 1万個ファイルがあると、全部見せるのに、1分間の時間がかかるであろう。
+  - 内部で、HTTP::Tinyを用いる。Net::Google::OAuthを使わない。
