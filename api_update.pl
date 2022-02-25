6eee81e4511556b266b8939fb44e9ade7e569363
Toshiyuki SHIMONO <bin4tsv@gmail.com>
2022-02-25 19:39:27 +0900

    api_update.pl 複数のファイルで動作するように変更。ヘルプの文面をかなり間違い訂正。他のファイルのコピペだった。このプログラムの動作はテストしていない。

--- api_update.pl
+++ api_update.pl
@@ -7,42 +7,47 @@ use URI::QueryParam ;
 use URI ;
 use Getopt::Std ; getopts 'i:',\my%o ;
 
-& HELP_MESSAGE if @ARGV < 3 ; 
-my $target_fileid = $ARGV[1] ;
+& HELP_MESSAGE if @ARGV == 0  ; 
+#my $target_fileid = $ARGV[0] ;
 
 my $GOOGLE_DRIVE_UPLOAD_API = "https://www.googleapis.com/upload/drive/v3/files/";
-my $ACCESS_TOKEN = $ARGV[0] ;
+my $gfile = '~/.gcpsetup2202/1' ; # GCPで使う合言葉を収めたファイルの名前
+my $ACCESS_TOKEN = qx [ sed -ne's/^ACCESS_TOKEN[ =:\t]*//p' $gfile ] =~ s/\n$//r ;
+chomp ( $ACCESS_TOKEN = <> ) if $o{'/'} ; # my $ACCESS_TOKEN = $ARGV[0] ;
 my $bearer = join ' ', ( 'Bearer', $ACCESS_TOKEN );
-my $URI = URI->new( $GOOGLE_DRIVE_UPLOAD_API . $target_fileid );
-$URI->query_param( uploadType => 'multipart' );
 
-my $ua  = LWP::UserAgent->new;
-my $res = $ua->request(
-  PATCH $URI,
-  'Content-Type' => 'multipart/form-data',
-  Authorization  => $bearer,
-  Content    => [
+& f_each ( split /:/, $_ , 2 ) for ( @ARGV ) ; 
 
-    metadata => [
-      undef, undef,
-      'Content-Type' => 'application/json;charset=UTF-8',
-      'Content'    => encode_json(
-        {
-          # name   => 'hogefuga.txt',
-          #  mimeType => 'plain/text',
-          #  parents  => ['10kCqEUmWsWlqMdP_vF9pDGrQXFVZ-Lvr'],
-          #  id => $target_fileid,
-        },
-      ),
+exit 0 ; 
+
+sub f_each ( $$ ) { 
+  my $URI = URI->new( $GOOGLE_DRIVE_UPLOAD_API . $_[0] );
+  $URI->query_param( uploadType => 'multipart' );
+  my $ua  = LWP::UserAgent->new;
+  my $res = $ua->request(
+    PATCH $URI,
+    'Content-Type' => 'multipart/form-data',
+    Authorization  => $bearer,
+    Content    => [
+      metadata => [
+        undef, undef,
+        'Content-Type' => 'application/json;charset=UTF-8',
+        'Content'    => encode_json(
+          {
+            # name   => 'hogefuga.txt',
+            #  mimeType => 'plain/text',
+            #  parents  => ['10kCqEUmWsWlqMdP_vF9pDGrQXFVZ-Lvr'],
+            #  id => $target_fileid,
+          },
+        ),
+      ],
+      file => [ $_[1] ] #["./hoge.txt"],
     ],
+  );
 
-    file => [ $ARGV[2] ] #["./hoge.txt"],
-  ],
-);
-
-print $res->code . "\n";
-print $res->content . "\n";
-
+  print $res->code . "\n";
+  print $res->content . "\n";
+}
 
 
 ## ヘルプ (オプション --help が与えられた時に、動作する)
@@ -63,15 +68,22 @@ sub HELP_MESSAGE {
 
 =head1
 
- $0  アクセストークン ファイルID ファイル名
+ $0  ファイルID:ファイル名 [ファイルID:ファイル名] [ファイルID:ファイル名] .. 
 
-  2番目はローカルのファイル名
   指定したファイルを指定したGoogleドライブのフォルダーにアップロードする。
-  同じ名前のファイルも複数回、このプログラムを実行すると、新規に次々とGoogleドライブにアップロードされる。少し要注意。
+  各引数の書式は次のようになる。
+    グーグルドライブ上のファイルID : ローカルのファイル名
 
- 前提的なこと: HTTP::Request::Common を用いる。Net::Google::OAuthを使わない。
+ 前提的なこと: 
+    このプログラムは、HTTP::Request::Common を用いる。Net::Google::OAuthを使っていない。
 
   オプション: 
+
+   -/ : アクセストークンを標準入力から読み取る。
+
+
+  開発上のメモ: 
+    複数のファイルに対応したい。
 
 
 # 200
