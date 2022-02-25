c450575d23eb080e4a9567f25bdddd23d15a1628
Toshiyuki SHIMONO <bin4tsv@gmail.com>
2022-02-25 21:57:01 +0900

    a2funcs.pl 出力形式も、合言葉の読取りの仕方も変更。

--- a2funcs.pl
+++ a2funcs.pl
@@ -4,25 +4,40 @@ use Data::Dumper;
 use Carp;
 use Net::Google::Drive ;
 use Getopt::Std ; getopts 'f:',\my%o ;
-$o{f} //= '*' ; # 探すファイルの名前(と思われる)
-
+$o{f} //= '*' ; # 探すファイルの名前(のようだ)
 binmode STDOUT, ":utf8"; # binmode STDIN,  ":utf8";
-
-my $CLIENT_ID = "545257978867-tt7539v8nuejtk6ng44en80l6334dseo.apps.googleusercontent.com" ;
-my $CLIENT_SECRET = "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" ; 
-my $REFRESH_TOKEN = "1//0e8i8kRu5P0PWCgYIARAAGA4SNwF-L9IrZ-F0zJbFcPqIWyVahL0Gtp5spr5yCPM5oXRszgU-SdEkdyVXuKLt8pPyLDJyrKxXNJY" ; 
-chomp (my $ACCESS_TOKEN = <> ) ; # chompはせずとも、動作はした。
+my $gfile = '~/.gcpsetup2202/1' ; # GCPで使う合言葉を収めたファイルの名前
+my $CLIENT_ID     = qx [ sed -ne's/^CLIENT_ID[ =:\t]*//p' $gfile ] =~ s/\n$//r ; #"54525797.....34dseo.apps.googleusercontent.com" ;
+my $CLIENT_SECRET = qx [ sed -ne's/^CLIENT_SECRET[ =:\t]*//p' $gfile ] =~ s/\n$//r ; # "GOCSP...YUbpe1" ; 
+my $REFRESH_TOKEN = qx [ sed -ne's/^REFRESH_TOKEN[ =:\t]*//p' $gfile ] =~ s/\n$//r ; 
+my $ACCESS_TOKEN  = qx [ sed -ne's/^ACCESS_TOKEN[ =:\t]*//p' $gfile ] =~ s/\n$//r ; 
 my $disk = Net::Google::Drive->new(  -client_id => $CLIENT_ID, -client_secret => $CLIENT_SECRET, -access_token  => $ACCESS_TOKEN, -refresh_token => $REFRESH_TOKEN, );
-
-# ファイル一覧
+# ファイル一覧を出力。
 my $file_name = $o{f} ; #'11_読み物/121010fukui.pdf';    # アスタリスクで全部のファイルの情報を取ってくる
 my $files = $disk->searchFileByNameContains( -filename => $file_name ) or croak "File '$file_name' not found";
 my $fnum = 0 ;
-for my $file ( @{$files} ) {
-    say '=' x 20 . sprintf (' %04d', ++ $fnum) ; say $file->{id}; say $file->{name}; say $file->{mimeType}; say $file->{kind};
-    say '=' x 20; # Googleドライブの、フォルダは、mimeTypeは、application/vnd.google-apps.folder になる。
-}
+#do { say '=' x 20 . sprintf (' %04d', ++ $fnum) ; say $_->{id}; say $_->{name}; say $_->{mimeType}; say $_->{kind}; say '=' x 20 } for @{$files} ;
+do { say join"\t",sprintf('%03d',++$fnum),$_->{kind},$_->{id},qq["$_->{name}"],$_->{mimeType} } for @{$files} ;
 
+# # ファイルダウンロード
+# # -dest_file ：保存先（ローカル）でのファイル名
+# # -file_id   ：ダウンロードしたいGoogle Drive上のコンテンツ
+# $disk->downloadFile(
+#     -dest_file => './branches.csv',
+#     -file_id   => '1juzsinLmryTufEIY53h3nuFPaq_l4Lxn',
+#     );
+# # ファイルアップロード
+# # -dest_file ：アップロードしたいファイル（のパス）
+# # -parents   ：Google Drive上のフォルダの下に格納するかを指定
+# # 無指定の場合はトップに保存
+# # フォルダIDはフォルダURLの後半のところ
+# # https://drive.google.com/drive/folders/{Google Drive上のフォルダID}
+
+# my $res = $disk->uploadFile(
+#     -source_file => './test.txt',
+#     -parents   => ['hogehogefuga'], # 配列リファレンスの中に指定すること
+#     );
+# # アップロードしたファイルの file_id などが返ってくる
 
 =encoding utf8
 
@@ -56,27 +71,3 @@ sub HELP_MESSAGE {
   exit 0 ;
 }
 
-
-# # ファイルダウンロード
-# # -dest_file ：保存先（ローカル）でのファイル名
-# # -file_id   ：ダウンロードしたいGoogle Drive上のコンテンツ
-# $disk->downloadFile(
-#     -dest_file => './branches.csv',
-#     -file_id   => '1juzsinLmryTufEIY53h3nuFPaq_l4Lxn',
-#     );
-
-
-# # ファイルアップロード
-# # -dest_file ：アップロードしたいファイル（のパス）
-# # -parents   ：Google Drive上のフォルダの下に格納するかを指定
-# # 無指定の場合はトップに保存
-# # フォルダIDはフォルダURLの後半のところ
-# # https://drive.google.com/drive/folders/{Google Drive上のフォルダID}
-
-# my $res = $disk->uploadFile(
-#     -source_file => './test.txt',
-#     -parents   => ['hogehogefuga'], # 配列リファレンスの中に指定すること
-#     );
-
-# # アップロードしたファイルの file_id などが返ってくる
-# say Dumper $res;
