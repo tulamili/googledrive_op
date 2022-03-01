5f786e056c9d04c1ded83530da558b548bfad13b
Toshiyuki SHIMONO <bin4tsv@gmail.com>
2022-03-01 14:33:57 +0900

    gdrv.pm 作成。共通する変数を格納するため。

---
+++ gdrv.pm
@@ -0,0 +1,13 @@
+#package App::googledrive_op::gdrv ; 
+#package main ; 
+package gdrv ; 
+our $gfile = '~/.gcpsetup2202/1' ; # GCPで使う合言葉を収めたファイルの名前
+
+#package gdrv ; 
+#use strict ; 
+#use warnings ; 
+use Exporter 'import' ; 
+
+#package gdrv ;
+our @EXPORT =  [ $main::gfile ] ; 
+1 ; 
