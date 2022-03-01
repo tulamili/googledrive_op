a846b1f3567f9e3236114223e38b3af5ea8c1bf6
Toshiyuki SHIMONO <bin4tsv@gmail.com>
2022-03-01 14:34:42 +0900

    download05.pl gdrv.pmを反映した書き方にした。他、変数名をリファクタしたり、気付いたことをリファクタ。

--- download05.pl
+++ download05.pl
@@ -1,21 +1,16 @@
 #!/usr/bin/perl
 #use 5.030 ; 
 use strict; use warnings ;
-use feature 'say' ;
-use Data::Dumper ;
-use Carp ;
+use Getopt::Std ; getopts '',\my%o ;
 use Net::Google::Drive ;
-use Getopt::Std ; getopts 'f:',\my%o ;
 & HELP_MESSAGE if @ARGV != 2 ;
-binmode STDOUT, ":utf8" ; # binmode STDIN,  ":utf8"　;
-my $gfile = '~/.gcpsetup2202/1' ; # GCPで使う合言葉を収めたファイルの名前
-my $CLIENT_ID     = qx [ sed -ne's/^CLIENT_ID[ =:\t]*//p' $gfile ] =~ s/\n$//r ; #"54525797.....34dseo.apps.googleusercontent.com" ;
-my $CLIENT_SECRET = qx [ sed -ne's/^CLIENT_SECRET[ =:\t]*//p' $gfile ] =~ s/\n$//r ; # "GOCSP...YUbpe1" ; 
-my $REFRESH_TOKEN = qx [ sed -ne's/^REFRESH_TOKEN[ =:\t]*//p' $gfile ] =~ s/\n$//r ; 
-my $ACCESS_TOKEN  = qx [ sed -ne's/^ACCESS_TOKEN[ =:\t]*//p' $gfile ] =~ s/\n$//r ; 
-my $disk = Net::Google::Drive->new(  -client_id => $CLIENT_ID, -client_secret => $CLIENT_SECRET, -access_token  => $ACCESS_TOKEN, -refresh_token => $REFRESH_TOKEN, );
-# ファイル一覧を出力。
-$disk->downloadFile( -file_id => $ARGV[0], -dest_file => $ARGV[1] ) or croak "Failure to download.";
+my $gfile = do { use FindBin qw [ $Bin ] ; use lib $FindBin::Bin ; use gdrv ; $gdrv::gfile } ;  # GCPで使う合言葉を収めたファイルの名前
+my $cid  = qx [ sed -ne's/^CLIENT_ID[ =:\t]*//p' $gfile ] =~ s/\n$//r ; #"54525797.....34dseo.apps.googleusercontent.com" ;
+my $csec = qx [ sed -ne's/^CLIENT_SECRET[ =:\t]*//p' $gfile ] =~ s/\n$//r ; # "GOCSP...YUbpe1" ; 
+my $rtoken = qx [ sed -ne's/^REFRESH_TOKEN[ =:\t]*//p' $gfile ] =~ s/\n$//r ; 
+my $atoken  = qx [ sed -ne's/^ACCESS_TOKEN[ =:\t]*//p' $gfile ] =~ s/\n$//r ; 
+my $disk = Net::Google::Drive->new( -client_id => $cid, -client_secret => $csec, -access_token => $atoken, -refresh_token => $rtoken );
+$disk->downloadFile( -file_id => $ARGV[0], -dest_file => $ARGV[1] ) or do { use Carp ; croak "Failure to download." } ;
 exit ; 
 
 # my $r = $disk->uploadFile(-dest_file => './test.txt', -parents   => ['folder'] ) ; # アップロードの場合. file_id などが返り値
